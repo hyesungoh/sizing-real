3afc65fc17e881abca17e3366c16d5b0d1a93b33
haesungoh414 <haesungoh414@gmail.com>
2017-08-12 00:51:50 +0900

    svg의 값을 변수로 빼 두었고 input 뷰에서 입력한 값을 비율로 계산하여 적용하였음
    해야할 것
    : 각 비율들의 계산 더욱 세밀히
    : input 뷰의 form 태그들 form_for로 고치는 작업
    : aws 디플로이

--- app/controllers/sizingreal_controller.rb
+++ app/controllers/sizingreal_controller.rb
@@ -20,11 +20,11 @@ class SizingrealController < ApplicationController
     "m_rise" => params[:m_rise],
     "m_tail" => params[:m_tail]}
 
-    @diff_length = (@hash_jean_size["length"]).to_f - (@hash_my_size["m_length"]).to_f
-    @diff_waist = (@hash_jean_size["waist"]).to_f - (@hash_my_size["m_waist"]).to_f
-    @diff_thigh = (@hash_jean_size["thigh"]).to_f - (@hash_my_size["m_thigh"]).to_f
-    @diff_rise = (@hash_jean_size["rise"]).to_f - (@hash_my_size["m_rise"]).to_f
-    @diff_tail = (@hash_jean_size["tail"]).to_f - (@hash_my_size["m_tail"]).to_f
+    @diff_length = (@hash_jean_size["length"]).to_f / (@hash_my_size["m_length"]).to_f
+    @diff_waist = (@hash_jean_size["waist"]).to_f / (@hash_my_size["m_waist"]).to_f
+    @diff_thigh = (@hash_jean_size["thigh"]).to_f / (@hash_my_size["m_thigh"]).to_f
+    @diff_rise = (@hash_jean_size["rise"]).to_f / (@hash_my_size["m_rise"]).to_f
+    @diff_tail = (@hash_jean_size["tail"]).to_f / (@hash_my_size["m_tail"]).to_f
 
   end
 
